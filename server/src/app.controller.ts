3f328bd8aba53ee5cfcdf25098fe7a2232ffd49d
mollgerman <germandmoll2004@gmail.com>
2023-11-06 13:50:52 -0300

    modify controller and use github service

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { GithubService } from './providers/github/github.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly githubService: GithubService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('github')
+  public async getGithub() {
+    return this.githubService.getCommits();
   }
 }
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ProvidersModule } from './providers/providers.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ProvidersModule],
+  controllers: [AppController]
 })
 export class AppModule {}
